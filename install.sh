b5c1fa927ffc8db68da6fb3df23454550ec4bf1b
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-23 12:20:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # Install packages on Fedora
         sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
         sudo dnf install -y ffmpeg nodejs git ImageMagick yarnpkg
-    elif [[ "$TERMUX_VERSION" != "" ]]; then
-        # Install packages on Termux
-        pkg install -y ffmpeg nodejs git imagemagick libwebp yarn
     elif [[ -f /etc/lsb-release && "$(cat /etc/lsb-release | grep DISTRIB_ID)" == *"Ubuntu"* ]]; then
         # Install packages on Ubuntu
         sudo apt-get update
@@ -39,6 +36,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Unknown Linux distribution"
         exit 1
     fi
+elif [[ "$TERMUX_VERSION" != "" ]]; then
+    # Install packages on Termux
+    pkg install -y ffmpeg nodejs git imagemagick libwebp yarn
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Install packages on macOS
     brew install ffmpeg nodejs git imagemagick yarn
