be1ae2d9c4cfd5ccd8a960823b7ecad715bb2300
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-23 21:35:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         sudo apt-get install -y ffmpeg git curl imagemagick
         curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
         sudo apt-get install -y nodejs
-        curl -o- -L https://yarnpkg.com/install.sh | bash
+        sudo apt install -y yarn
     elif [[ -f /etc/debian_version ]]; then
         # Install packages on Debian
         sudo apt-get update
         sudo apt-get install -y ffmpeg git curl imagemagick
         curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
         sudo apt-get install -y nodejs
-        curl -o- -L https://yarnpkg.com/install.sh | bash
+        sudo apt install -y yarn
     elif [[ -f /etc/redhat-release ]]; then
         # Install packages on Red Hat
         sudo yum install -y ffmpeg nodejs git imagemagick yarn
