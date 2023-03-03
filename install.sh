6d11397e637722c7ac58f69c552d553f5a4de3f7
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-03-03 22:14:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,22 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Check for specific Linux distributions
     if [[ -f /etc/arch-release ]]; then
         # Install packages on Arch Linux
+        echo "Detect OS :- Arch Linux" 
+        sleep 3       
         sudo pacman -Syu --noconfirm ffmpeg nodejs-lts-hydrogen git imagemagick npm
         sudo npm install --global yarn
         clear
     elif [[ -f /etc/fedora-release ]]; then
         # Install packages on Fedora
+        echo "Detect OS :- Fedora Linux" 
+        sleep 3 
         sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
         sudo dnf install -y ffmpeg nodejs git ImageMagick yarnpkg
+        clear
     elif [[ -f /etc/lsb-release && "$(cat /etc/lsb-release | grep DISTRIB_ID)" == *"Ubuntu"* ]]; then
         # Install packages on Ubuntu
+        echo "Detect OS :- Ubuntu Linux"
+        sleep 3 
         sudo apt-get update
         sudo apt-get install -y ffmpeg git curl imagemagick
         curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
@@ -23,6 +30,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         clear
     elif [[ -f /etc/debian_version ]]; then
         # Install packages on Debian
+        echo "Detect OS :- Debian Linux"
+        sleep 3 
         sudo apt-get update
         sudo apt-get install -y ffmpeg git curl imagemagick
         curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
@@ -32,6 +41,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         clear
     elif [[ -f /etc/redhat-release ]]; then
         # Install packages on Red Hat
+        echo "Detect OS :- RedHat Linux"
+        sleep 3 
         sudo yum install -y ffmpeg nodejs git ImageMagick yarnpkg
         clear
     else
@@ -45,6 +56,8 @@ elif [[ "$OSTYPE" == "linux-android"* ]]; then
 elif [[ "$OSTYPE" == "linux"* ]]; then
     if [[ -f /etc/os-release && "$(cat /etc/os-release | grep ID_LIKE)" == *"suse"* ]]; then
         # Install packages on openSUSE
+        echo "Detect OS :- openSUSE Linux" 
+        sleep 3
         sudo zypper install -y ffmpeg-5 nodejs18 git ImageMagick 
         sudo zypper install -y nodejs18 git ImageMagick ffmpeg-4
         sudo npm install --global yarn
